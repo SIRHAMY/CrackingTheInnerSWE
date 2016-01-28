6184fe42b5e7ae721a9af50c48cbf12022d6965a
Hamilton Greene <hamiltongreene@BPSs-MacBook-Pro.local>
2016-01-28 14:05:51 -0500

    Updated 4.7 solution with function that removes need for parent links

--- 4-7-FindAncestor/src/Finder.java
+++ 4-7-FindAncestor/src/Finder.java
@@ -23,15 +23,19 @@ public class Finder {
 		
 		System.out.println("D and E - Expected B: 2");
 		System.out.println(find.findAncestor(d, e).getData());
+		System.out.println(find.findAncestorNoParent(a, d, e).getData());
 		
 		System.out.println("D and F - Expected A: 1");
 		System.out.println(find.findAncestor(d, f).getData());
+		System.out.println(find.findAncestorNoParent(a, d, f).getData());
 		
 		System.out.println("B and A - Expected A: 1");
 		System.out.println(find.findAncestor(b, a).getData());
+		System.out.println(find.findAncestorNoParent(a, b, a).getData());
 		
 		System.out.println("A and C - Expected A: 1");
 		System.out.println(find.findAncestor(a, c).getData());
+		System.out.println(find.findAncestorNoParent(a, a, c).getData());
 	}
 	
 	public BSTNode findAncestor(BSTNode a, BSTNode b) {
@@ -68,5 +72,34 @@ public class Finder {
 		if(root.getParent() != null) return searchParent(root.getParent(), target, root);
 		
 		return null;
+	}
+	
+	public BSTNode findAncestorNoParent(BSTNode root, BSTNode a, BSTNode b) {
+		return findAncestorHelperNoParent(root, a, b);
+	}
+	
+	public BSTNode findAncestorHelperNoParent(BSTNode root, BSTNode a, BSTNode b) {
+		if(root == a) return root;
+		if(root == b) return root;
+		
+		BSTNode left = null;
+		if(root.getLeft() != null) left = findAncestorHelperNoParent(root.getLeft(), a, b);
+		
+		BSTNode right = null;
+		if(root.getRight() != null) right = findAncestorHelperNoParent(root.getRight(), a, b);
+		
+		if(left != null && right != null) {
+			if( ( left == a && right == b ) || ( left == b && right == a ) ) {
+				return root;
+			} else if ( left != a && left != b) return left;
+			else if ( right != a && right != b) return right;
+		} else if(left != null) {
+			return left;
+		} else if(right != null) {
+			return right;
+		} 
+		
+		return null;
+		
 	}
 }
