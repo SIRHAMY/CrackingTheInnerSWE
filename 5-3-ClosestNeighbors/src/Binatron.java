f60ade56dc3af88d879e991c4ba7dc8f870da665
Hamilton Greene <hamilton.greene@gmail.com>
2016-02-08 23:00:41 -0500

    Got nextSmallest to work, but nextLargest is still broken

--- 5-3-ClosestNeighbors/src/Binatron.java
+++ 5-3-ClosestNeighbors/src/Binatron.java
@@ -3,49 +3,70 @@ public class Binatron {
 	public static void main(String[] args) {
 		Binatron b = new Binatron();
 		
-		int test = 1101;
+		int setBitTest = 0b11;
+		System.out.println("Expected 10 - Actual " + b.clearBit(setBitTest, 0));
+		
+		int test = 0b1101;
 		System.out.println("Test: " + test);
 		System.out.println("Next Smallest: " + b.nextSmallest(test));
-		System.out.println("Next Biggest: " + b.nextLargest(test));
+		System.out.println("Next Biggest: " + b.nextLargest(0b1101));
 	}
 
 	public double nextSmallest(int enter) {
+		
+		System.out.println("Debug: Smallest");
 		
 		int base = enter;
 		int length = (int)(Math.log10(base) + 1);
 		
 		for(int i = 0; i < length-1; i++) {
 			if(!getBit(base, i)) {
+				System.out.println("Debug: 0 found at index: " + i);
+				
 				if(getBit(base, i + 1)) {
+					System.out.println("Debug: Found 1 next to it");
+					System.out.println("Debug: Base Before = " + base);
+					
 					//Swap ze bits
-					setBit(base, i + 1, 0);
-					setBit(base, i, 1);
+					base = clearBit(base, i + 1);
+					base = setBit(base, i);
+					
+					System.out.println("Debug: Base after = " + base);
+					
 					return base;
 				}
 			}
 		}
 		
-		setBit(base, 0, 0);
+		base = clearBit(base, 0);
 		return base += 0.1;
 	}
 	
 	public double nextLargest(int enter) {
+		System.out.println("Largest");
+		
 		int base = enter;
 		int length = (int)(Math.log10(base) + 1);
 		
-		for(int i = 0; i < length-1; i++) {
+		System.out.println("Debug: Enter = " + base);
+		
+		for(int i = 0; i < length; i++) {
 			if(getBit(base, i)) {
+				System.out.println("Debug: Found a 1 at index: " + i);
 				if(!getBit(base, i + 1)) {
+					System.out.println("And there's a 0 here at index" + (i+1));
 					//Swap ze bits
-					setBit(base, i + 1, 0);
-					setBit(base, i, 1);
+					base = setBit(base, i + 1);
+					base = clearBit(base, i);
+					
+					System.out.println("Debug: changed = " + base);
 					return base;
 				}
 			}
 		}
 		
-		setBit(base, length + 1, 1);
-		setBit(base, length, 0);
+		base = setBit(base, length + 1);
+		base = clearBit(base, length);
 		return base;
 	}
 	
@@ -58,10 +79,12 @@ public class Binatron {
 		return mask > 0;
 	}
 	
-	public void setBit(int base, int index, int value) {
-		int mask = 0b0 | value;
-		mask = mask << index;
-		
-		base = base | mask;
+	public int setBit(int base, int index) {
+		return base | (0b1<<base);
+	}
+	
+	public int clearBit(int base, int index) {
+		int mask = ~(0b1 << index);
+		return base & mask;
 	}
 }
