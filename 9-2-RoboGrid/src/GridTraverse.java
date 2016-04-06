f02c40543cbf4f5d8c46a7538a53bc3ccdf34e5b
Hamilton Greene <hamilton.greene@gmail.com>
2016-04-06 14:27:49 -0400

    First stabs at 9.2 additional work

--- 9-2-RoboGrid/src/GridTraverse.java
+++ 9-2-RoboGrid/src/GridTraverse.java
@@ -1,16 +1,31 @@
+import java.util.ArrayList;
 
 public class GridTraverse {
 	
 	public static void main(String[] args) {
 		System.out.println("GridTraverse Test");
 		
-		GridTraverse grid = new GridTraverse();
+		GridTraverse traverse = new GridTraverse();
 		
 		for(int run = 0; run<10; run++) {
 			System.out.println("Run #" + run);
 			System.out.println("Coordinates (x,y) = (" + run + "," + run + ")");
-			System.out.println("Output: " + grid.recursivePossiblePaths(0,0,run,run));
+			System.out.println("Output: " + traverse.recursivePossiblePaths(0,0,run,run));
 		}
+		
+		Grid blockage = new Grid(6, 6);
+		blockage.setBlocked(1, 1, true);
+		blockage.setBlocked(2, 2, true);
+		
+		System.out.println("GridTraverse Test with Blocked tiles");
+		for(int run = 0; run<10; run++) {
+			System.out.println("Run #" + run);
+			System.out.println("Coordinates (x,y) = (" + run + "," + run + ")");
+			System.out.println("Output: " + traverse.recursivePossiblePaths(0,0,run,run, blockage));
+		}
+		
+		System.out.println("Debug: " + blockage.printGrid());
+		
 	}
 
 	/*Input the coordinates of the start and end point, get back the number of paths 
@@ -22,6 +37,22 @@ public class GridTraverse {
 		int paths = 0;
 		if(xStart < xEnd) paths += recursivePossiblePaths(xStart + 1, yStart, xEnd, yEnd);
 		if(yStart < yEnd) paths += recursivePossiblePaths(xStart, yStart + 1, xEnd, yEnd);
+		
+		return paths;
+	}
+	
+	/*
+	 * This implementation gives the option to pass in an array of "blocked" coordinates
+	 * that the robot can't traverse over
+	 */
+	public int recursivePossiblePaths(int xStart, int yStart, int xEnd, int yEnd, Grid blocked) {
+		if(xStart == xEnd && yStart == yEnd) return 1;
+		
+		int paths = 0;
+		if(xStart < xEnd && !blocked.isBlocked(xStart + 1, yStart)) 
+			paths += recursivePossiblePaths(xStart + 1, yStart, xEnd, yEnd);
+		if(yStart < yEnd && !blocked.isBlocked(xStart, yStart + 1)) 
+			paths += recursivePossiblePaths(xStart, yStart + 1, xEnd, yEnd);
 		
 		return paths;
 	}
